fe6a471ad24d78edf6940c7c109d415cefff1830
Nilou Houshmand <nilou.houshmand@gmail.com>
2021-11-18 21:55:14 -0800

    refactoring comment js to be dynamically

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,10 +1,34 @@
-//Declare Variables
-const form = document.querySelector(".form");
+let comments = [
+  {
+    fanName: "Connor Walton",
+    timestamp: "02/17/2021",
+    text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+
+  {
+    fanName: "Emilie Beach",
+    timestamp: "01/09/2021",
+    text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+
+  {
+    fanName: "Miles Acosta",
+    timestamp: "12/20/2020",
+    text: "Lead EducatorI can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
+
 const commentsBody = document.querySelector(".comments");
 
-function commentGenerator(fanName, message) {
+for (let i = 0; i < comments.length; i++) {
+  let comment = comments[i];
+  let commentsItem = displayComment(comment);
+  commentsBody.appendChild(commentsItem);
+}
+
+function displayComment(comment) {
   //Create comment row
-  const commentsItem = document.createElement("div");
+  let commentsItem = document.createElement("div");
   commentsItem.classList.add("comments__item");
 
   //Create comment avatar
@@ -18,35 +42,66 @@ function commentGenerator(fanName, message) {
   //Create Pragraph for comment name
   const commentsName = document.createElement("p");
   commentsName.classList.add("comments__name");
-  commentsName.innerHTML = fanName;
+  commentsName.innerHTML = comment.fanName;
 
   //Create Pragraph for comment post time
   const commentsTime = document.createElement("span");
   commentsTime.classList.add("comments__time");
-  commentsTime.innerText = "02/17/2021";
+  commentsTime.innerText = comment.timestamp;
 
   //Create comment message
   const commentsComment = document.createElement("p");
   commentsComment.classList.add("comments__comment");
-  commentsComment.innerText = message;
+  commentsComment.innerText = comment.text;
 
   //Adding our Elements to HTML
-  commentsBody.prepend(commentsItem);
   commentsItem.appendChild(commentsAvatar);
   commentsItem.appendChild(commentsMessage);
   commentsMessage.appendChild(commentsName);
   commentsName.appendChild(commentsTime);
   commentsMessage.appendChild(commentsComment);
+
+  return commentsItem;
 }
 
-// Function to Handle on submitting the form
-const formHandler = (e) => {
-  e.preventDefault();
-  const fanName = e.target.name.value;
-  const message = e.target.comment.value;
+// // Function to Handle on submitting the form
+// const form = document.querySelector(".form");
+// const formHandler = (e) => {
+//   e.preventDefault();
 
-  //e.target.reset();
-  commentGenerator(fanName, message);
-};
+//   let newComment = {
+//     fanName: e.target.name.value,
+//     timestamp: "2021/01/01",
+//     text: e.target.commentTextArea.value,
+//   };
 
-form.addEventListener("submit", formHandler);
+//   comments.push(newComment);
+//   console.log(comments);
+
+//   e.target.reset();
+//   displayComment(newComment);
+// };
+
+// form.addEventListener("submit", formHandler);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".form");
+
+  const formHandler = (e) => {
+    e.preventDefault();
+
+    let newComment = {
+      fanName: e.target.name.value,
+      timestamp: "2021/01/01",
+      text: e.target.commentTextArea.value,
+    };
+
+    comments.unshift(newComment);
+    console.log(comments);
+
+    e.target.reset();
+    let commentsItem = displayComment(newComment);
+    commentsBody.prepend(commentsItem);
+  };
+  form.addEventListener("submit", formHandler);
+});
